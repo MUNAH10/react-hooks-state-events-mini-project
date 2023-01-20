ab05fb6f7a24607af922b523c55a87273c5d0cc0
MUNAH10 <munahassan5075@gmail.com>
2023-01-20 22:10:26 +0300

    completed

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,12 +1,19 @@
 import React from "react";
 
-function CategoryFilter() {
+function CategoryFilter({categories,click}) {
+export default function CategoryFilter({categories,click}) {
+  const buttons = categories.map((category, ind) => {
+    return (
+      <button key={ind} className={category} onClick={() => click(category)} >{category} </button>
+    )
+  });
   return (
     <div className="categories">
       <h5>Category filters</h5>
       {/* render <button> elements for each category here */}
+      {buttons}
     </div>
   );
 }
 
-export default CategoryFilter;
+// export default CategoryFilter;
